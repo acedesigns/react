b9461781b36b64c1df7369d8633be899c874c278
Ace Designs <acedesigns123@gmail.com>
2020-05-21 01:47:06 +0200

    deply app

---
+++ app/precache-manifest.b991d1de1620dfc4405714b55f093c2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f9200824dda896f692dfc4b5ddb1b2",
+    "url": "/acedesigns/react/index.html"
+  },
+  {
+    "revision": "52df05e768d1aa263669",
+    "url": "/acedesigns/react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e1eec540731ec24684f7",
+    "url": "/acedesigns/react/static/css/main.ce37aefb.chunk.css"
+  },
+  {
+    "revision": "52df05e768d1aa263669",
+    "url": "/acedesigns/react/static/js/2.46920cfe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/acedesigns/react/static/js/2.46920cfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1eec540731ec24684f7",
+    "url": "/acedesigns/react/static/js/main.d329cac1.chunk.js"
+  },
+  {
+    "revision": "52bb77d64bdba0d1f1cc",
+    "url": "/acedesigns/react/static/js/runtime-main.d7c1bde9.js"
+  }
+]);
