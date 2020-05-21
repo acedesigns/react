8580b03970a813f211ff54df21971f73129e878f
Ace Designs <acedesigns123@gmail.com>
2020-05-21 02:06:16 +0200

    Updates

---
+++ precache-manifest.77f01d8cadcb452f830cedbe1260010a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3660fa97a84d7092724e47b8a7e0a7",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "52df05e768d1aa263669",
+    "url": "/react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d7f77c7d31edff771729",
+    "url": "/react/static/css/main.ce37aefb.chunk.css"
+  },
+  {
+    "revision": "52df05e768d1aa263669",
+    "url": "/react/static/js/2.46920cfe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react/static/js/2.46920cfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f77c7d31edff771729",
+    "url": "/react/static/js/main.cbda4c85.chunk.js"
+  },
+  {
+    "revision": "9f419f06011e21f745b4",
+    "url": "/react/static/js/runtime-main.8d7da891.js"
+  }
+]);
