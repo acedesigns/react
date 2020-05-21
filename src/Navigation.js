efbf6d4dc2a78b4828c171a13b7258bfaecb19a6
Ace Designs <acedesigns123@gmail.com>
2020-05-21 03:24:54 +0200

    home link1

--- src/Navigation.js
+++ src/Navigation.js
@@ -15,7 +15,7 @@ function Navigation () {
     return(
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
-                <a className="navbar-brand" href="#">App</a>
+                <a className="navbar-brand" href="/">App</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -23,7 +23,7 @@ function Navigation () {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
-                            <Link className="nav-link" basename={process.env.PUBLIC_URL}>Home</Link>
+                            <Link className="nav-link" to="/">Home</Link>
                         </li>
 
                         <li className="nav-item">
@@ -38,7 +38,13 @@ function Navigation () {
                 </div>
             </nav>
 
-
+            <div className="jumbotron">
+                <div className="container">
+                    <h1 className="display-6">Hello, world!</h1>
+                    <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+                    <p><a className="btn btn-primary btn-sm" href="#" role="button">Learn more &raquo;</a></p>
+                </div>
+            </div>
         </div>
     )
 }
